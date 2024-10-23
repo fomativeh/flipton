d7e58003bfdc4671c98562f4e686baa297c7aff4
fomativeh <ufomaitive@gmail.com>
2024-10-23 18:35:22 +0100

    Updated tonconnect config

--- src/app/components/CSRRootLayout.tsx
+++ src/app/components/CSRRootLayout.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 const CSRRootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <TonConnectUIProvider manifestUrl="https://flipton-beta.vercel.app/tonconnect-manifest.json">
+    <TonConnectUIProvider manifestUrl="https://flipton-test.vercel.app/tonconnect-manifest.json">
       <SDKProvider>
         <section className="app-wrap max-w-[500px] min-w-[350px] min-h-[100vh] w-[100vw] ">
           {children}
