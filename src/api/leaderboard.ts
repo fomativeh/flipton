92fdb6cd18f187a0fddd73c2c247a89cb6aa709e
Itive Peace <86773959+fomativeh@users.noreply.github.com>
2025-04-13 15:58:15 +0200

    Update leaderboard.ts

--- src/api/leaderboard.ts
+++ src/api/leaderboard.ts
@@ -3,7 +3,7 @@ const API_BASE_URL = process.env.NEXT_PUBLIC_BOT_SERVER_URL
 
 export const fetchLeaderboard = async (token: string) => {
     try {
-        const res: any = await callEndpoint(API_BASE_URL, `/leaderboard`, "GET", {}, token)
+        const res: any = await callEndpoint(API_BASE_URL, `/game/leaderboard`, "GET", {}, token)
         if (res?.data?.success) {
             return { success: true, data: res?.data?.data }
         }
